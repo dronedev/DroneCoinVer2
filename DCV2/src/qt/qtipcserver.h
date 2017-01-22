3615ae62544225130f4aa7dcd2bd3213e7ff0247
dronedev <dronecoindev@gmx.com>
2017-01-22 18:40:51 -0500

    DroneCoin Version 2
    Ver2

---
+++ DCV2/src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define DroneCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "DroneCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
